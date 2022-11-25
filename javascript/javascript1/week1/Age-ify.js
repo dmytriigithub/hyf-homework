f36e8bff803af815a2cdb7e6ad119703706a7c19
Dmytrii Datskevych <datskevychdv@gmail.com>
2022-11-25 20:55:14 +0100

    Add Age-ify

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,6 @@
+// A future age calculator
+
+const yearOfBirth = 1880;
+const yearFuture = 2050;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
