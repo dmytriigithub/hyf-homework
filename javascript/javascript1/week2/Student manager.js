82087e9e7af3a6263f928b9a7e34f6e82e98a276
Dmytrii Datskevych <datskevychdv@gmail.com>
2022-12-02 10:29:16 +0100

    Changed Student manager

--- javascript/javascript1/week2/Student manager.js
+++ javascript/javascript1/week2/Student manager.js
@@ -1,22 +1,36 @@
-const class07Students = ['John', 'Jack', 'Dick', 'Bob', 'Jim', 'Tim'];
+const class07Students = [];
 function addStudentToClass(studentName) {
-    for (let i = 0; i < class07Students.length; i++){
-        if (studentName === class07Students[i])
-            return `Student ${studentName} is already in the class`;
+    if (class07Students.length < 6) {
+        if (studentName === '') {
+            return `You cannot add an empty string to a class`;
+        }else {
+            for (let i = 0; i < class07Students.length; i++){
+                if (studentName === class07Students[i]){
+                    return `Student ${studentName} is already in the class`;
+                }
+            }
         }
-
-    if (studentName === 'Margrethe II' && class07Students.length == 6) {
-        class07Students.pop();
-    }else if (studentName === '') {
-        return `You cannot add an empty string to a class`;
-    }else if (class07Students.length == 6) {
-        return `Cannot add more students to class 07`;
+    }else {
+        if (studentName === 'Margrethe II') {
+            class07Students.pop();
+        }else if (class07Students.length == 6) {
+            return `Cannot add more students to class 07`;
+            }
     }
+    
     
     class07Students.push(studentName);
     return class07Students;
 }
 
+console.log(addStudentToClass('John'));
+console.log(addStudentToClass('Jack'));
+console.log(addStudentToClass('Dick'));
+console.log(addStudentToClass('Dick'));
+console.log(addStudentToClass('Bob'));
+console.log(addStudentToClass('Jim'));
+console.log(addStudentToClass(''));
+console.log(addStudentToClass('Tim'));
 console.log(addStudentToClass('Margrethe II'));
 
 
