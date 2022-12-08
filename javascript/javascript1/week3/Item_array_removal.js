26f25050e816a64dabb704765f5a31372c0a31c7
Dmytrii Datskevych <datskevychdv@gmail.com>
2022-12-08 16:04:41 +0100

    Add Item_array_removal

---
+++ javascript/javascript1/week3/Item_array_removal.js
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------------------Item array removal
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+const nameToRemove = "Ahmad";
+  
+// Write some code here
+
+names.splice(names.indexOf(nameToRemove), 1);
+
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
