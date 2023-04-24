319cc62e2c3f66562f658b2c64f209d5a781adc9
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-04-24 23:13:42 +0200

    Added using the api

--- react/react1/week3/Todolist/src/components/todoApp/App.jsx
+++ react/react1/week3/Todolist/src/components/todoApp/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 
 import TodoAppList from '../todoAppList/todoAppList'
 import TodoForm from '../todoForm/TodoForm';
 import TodoTimer from '../todoTimer/TodoTimer';
-import './App.css'
-
-import todos from '../../todos';
+import './App.css';
 
 
 const App = () => {
 
-  const [todo, setTodo] = useState(todos);
+  const [todo, setTodo] = useState([]);
+  const [error, setError] = useState(null);
   const [maxId, setMaxId] = useState(4);
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw")
+    .then(resp => resp.json())
+    .then(data => {
+      setTodo(data);
+      setError(null);
+    })
+    .catch(error => {
+      console.error("Error fetching todolist:", error);
+      setError("Failed to fetch todolist. Please try again later.");
+      setTodo(null);
+    });
+  }, []);
 
   const deleteItem = (id) => {
     const updatedTodo = todo.filter(item => item.id !== id);
@@ -66,13 +79,15 @@ const App = () => {
     <div className="app">
       <TodoTimer/>
       <TodoForm onAdd={addItem}/>
-      <TodoAppList 
+      {error && <div className="error">{error}</div>}
+      {todo && <TodoAppList 
         
         todos={todo} 
         onDelete={deleteItem} 
         onToggleComplete={onToggleComplete}
         onEdit={editItem}
-        onUpdate={updateItem}/>
+        onUpdate={updateItem}/>}
+      
         
     </div>
   )
