762313a42ee7e287d223fccbd9f183a141c763a2
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-05-01 21:15:33 +0200

    Added changes after review

--- react/react1/week3/Todolist/src/components/item/Item.jsx
+++ react/react1/week3/Todolist/src/components/item/Item.jsx
@@ -1,14 +1,10 @@
-import React from "react";
 import './Item.css';
 
 const Item = (props) => {
     return (
         <div className="todo-list">
-            <h1 className={!props.children.length ? 'empty-list': 'empty-list hide'}>"No items..."</h1>
             <ul className="list">
-                {React.Children.map(props.children, child => {
-                    return React.cloneElement(child, {className:"list-item"})
-                })}
+            {props.children.length ? props.children : <h1 className='empty-list'>"No items..."</h1>}
             </ul>
         </div>
     )
